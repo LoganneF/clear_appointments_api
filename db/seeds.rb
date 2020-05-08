37780ddfbe1ab8baedecf64c2486026854fd0d9e
LoganneF <loganne.featherston@gmail.com>
2020-05-08 10:24:29 -0600

    successfully seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Patient.create([
+    { name: 'Loganne', DOB: '5/27/96', insurance: 'Providence', allergies: 'gluten' },
+    { name: 'Morgan', DOB: '08/19/93', insurance: 'Blue Cross', allergies: 'none' }
+])
+
+Doctor.create([
+    { name: 'Dr. Klien', specialty: 'Optometrist', insurance: 'Providence' },
+    { name: 'Dr. Reed', specialty: 'Pediatrist', insurance: 'Blue Cross' },
+])
+
+puts "seeded database"
